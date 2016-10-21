fbca0630211a2346d520f1027dbeb2e72137b0cc
Android Developer <developer79433@gmail.com>
2016-10-22 00:07:50 +1100

    Silence warning in abstract base class method call.

--- mrz.h
+++ mrz.h
@@ -30,8 +30,8 @@ public:
         return 0;
     };
     static const char *charset;
-    static unsigned int getCharsPerLine(void) { assert(0); };
-    static unsigned int getLineCount(void) { assert(0); };
+    static unsigned int getCharsPerLine(void) { assert(0); return 0; };
+    static unsigned int getLineCount(void) { assert(0); return 0; };
 };
 
 class MRZType1 : public MRZ {
