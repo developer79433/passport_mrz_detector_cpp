d4a8a15a7fcb53abc76e14e9bfa3112e94b12ede
Android Developer <developer79433@gmail.com>
2016-10-22 11:02:59 +1100

    Delete files moved to libmyocr.

--- find_borders.h
+++
@@ -1,8 +0,0 @@
-#ifndef FIND_BORDERS_H
-#define FIND_BORDERS_H 1
-
-#include <opencv2/opencv.hpp>
-
-cv::Rect find_borders(const cv::Mat &image);
-
-#endif /* FIND_BORDERS_H */
--- getcwd.h
+++
@@ -1,8 +0,0 @@
-#ifndef GETCWD_H
-#define GETCWD_H 1
-
-#include <string>
-
-std::string getcwd(void);
-
-#endif /* GETCWD_H */
